032972c933bd16c0794be9c01cce8802f7ce5d03
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-09-16 11:41:01 -0400

    create and setup server

---
+++ server.js
@@ -0,0 +1,23 @@
+// Load Express
+const express = require('express');
+
+// Create Express app
+const app = express();
+
+// Set View Engine to jsx
+app.set('view engine', 'jsx');
+
+// Set views
+// app.set('views', './views') // didn't test
+
+// Root
+app.get('/', function(req, res){
+    res.send('<h1>Hello Pokemon!</h1>'); // without jsx
+    // res.render('index.jsx');
+})
+
+// Listen
+app.listen(3000, () => {
+    console.log("Listening port 3000");
+})
+
