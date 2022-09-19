118c9da6c55b9e22f3154a450c6b32f9ed219f6e
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-09-18 23:43:25 -0400

    index shows list of pokemon

--- server.js
+++ server.js
@@ -10,12 +10,17 @@ app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
 // Set views
-// app.set('views', './views') // didn't test
+app.set('views', './views') // doesn't work without this
+
+// Set model
+const pokemon = require('./models/pokemon.js'); //NOTE: it must start with ./ if it's just a file, not an NPM package
 
 // Root
 app.get('/', function(req, res){
-    res.send('<h1>Hello Pokemon!</h1>'); // without jsx
-    // res.render('index.jsx');
+    // res.send('<h1>Hello Pokemon!</h1>'); // without jsx
+    // res.render('Index.jsx');
+    // res.send(fruits);
+    res.render('Index.jsx', {pokemon: pokemon})
 })
 
 // Listen
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,1 +1,24 @@
 const React = require("react"); // must have
+
+class Index extends React.Component {
+    render() {
+        // Put props obj to destructure
+        const { pokemon } = this.props;
+
+        // Return
+        return (
+        <div>
+            <h1>Pokemon Index</h1>
+            <ul>
+                {pokemon.map((poke, i) => {
+                    return (
+                        <li>{poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}</li>
+                    )
+                })}
+            </ul>
+        </div>
+        );
+    }
+}
+
+module.exports = Index;
