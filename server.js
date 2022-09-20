bdff69b106cfec37c5a71783b9f52a0f0c55b28e
MNGiit <36828920+MNGiit@users.noreply.github.com>
2022-09-19 23:00:32 -0400

    change route

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const express = require('express');
 // Create Express app
 const app = express();
 
-// Set View Engine to jsx
+// Set View Engine to jsx // Middleware
 app.set('view engine', 'jsx');
 // Create Engine
 app.engine('jsx', require('express-react-views').createEngine());
+// Will execute before the completion of the req-resCycle
+// app.use((req, ers, next) => {
+//     console.log('I run for all routes');
+//     next();
+// })
+// Parses url encoded data to be used within application
+// app.use(express.urlencoded({extended:false}));
+
 
 // Set views
 app.set('views', './views') // doesn't work without this
@@ -16,7 +24,7 @@ app.set('views', './views') // doesn't work without this
 const pokemon = require('./models/pokemon.js'); //NOTE: it must start with ./ if it's just a file, not an NPM package
 
 // Root
-app.get('/', function(req, res){
+app.get('/pokemon', function(req, res){
     // res.send('<h1>Hello Pokemon!</h1>'); // without jsx
     // res.render('Index.jsx');
     // res.send(fruits);
